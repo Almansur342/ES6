24a1099a336f670983b33e2e27b011972c2d7814
Almansur342 <abdullahalmansur2002@gmail.com>
2024-02-25 19:08:09 +0600

    spreadArray practic done

---
+++ spreadArray.js
@@ -0,0 +1,19 @@
+// const number = [2, 4, 1];
+// const number1 = [...number];
+// // const number1 = [];
+// // for(let num of number){
+// //   number1.push(num);
+// // }
+
+// number.push(10);
+// number1.push(20);
+// console.log(number);
+// console.log(number1);
+
+const nums = [3, 5 ,2, 6, 4, 5, 6];
+// const a = nums[0];
+// const b = nums[1];
+// const c = nums[2];
+
+const [a, b, c, ...d] = nums
+console.log(a, b, c, d,)
