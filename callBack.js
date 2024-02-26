389a5e2df0b45ef9d6854ee148f559ec280c883e
Almansur342 <abdullahalmansur2002@gmail.com>
2024-02-26 10:43:42 +0600

    slosure done

--- callBack.js
+++ callBack.js
@@ -1,1 +1,12 @@
- 
+ function show(name, age){
+  console.log("my first name is ", name, "my age is ", age);
+ }
+
+ function fun1(name ,age, callback){
+  console.log("hello from fisrt function");
+  setTimeout(() =>{
+    callback(name, age)
+  },2000)
+
+ }
+ fun1("mahed", 23, show)
